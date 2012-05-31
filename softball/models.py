5764b80b574e7ea13f85217c5b5ae774ca10f280
Brad Milne <brad.milne@getelevate.com>
2012-05-31 12:24:35 -0400

    Models updated

--- softball/models.py
+++ softball/models.py
@@ -45,7 +45,38 @@ class Player(models.Model):
     position = models.CharField(max_length=2, choices=POSITION_CHOICES)
     pitches = models.CharField(max_length=1, choices=PITCHES_CHOICES)
     hometown = models.CharField(max_length=32)
-    
+
+    def avg(self, season_name):
+        sum_atbats = self.statline_set.filter(season_name=season_name).aggregate(Sum('at_bats')).values()[0]
+        sum_singles = self.statline_set.filter(season_name=season_name).aggregate(Sum('singles')).values()[0]
+        sum_doubles = self.statline_set.filter(season_name=season_name).aggregate(Sum('doubles')).values()[0]
+        sum_triples = self.statline_set.filter(season_name=season_name).aggregate(Sum('triples')).values()[0]
+        sum_homeruns = self.statline_set.filter(season_name=season_name).aggregate(Sum('homeruns')).values()[0]  
+        avg = (sum_singles + sum_doubles + sum_triples + sum_homeruns) / float(sum_atbats)  
+        return avg
+		
+    def slg(self, season_name):
+	    sum_atbats = self.statline_set.filter(season_name=season_name).aggregate(Sum('at_bats')).values()[0]
+	    sum_singles = self.statline_set.filter(season_name=season_name).aggregate(Sum('singles')).values()[0]
+	    sum_doubles = self.statline_set.filter(season_name=season_name).aggregate(Sum('doubles')).values()[0]
+	    sum_triples = self.statline_set.filter(season_name=season_name).aggregate(Sum('triples')).values()[0]
+	    sum_homeruns = self.statline_set.filter(season_name=season_name).aggregate(Sum('homeruns')).values()[0]  
+	    slg = (sum_singles*1 + sum_doubles*2 + sum_triples*3 + sum_homeruns*4) / float(sum_atbats)  
+	    return slg
+	
+    def woba(self, season_name):
+	    sum_atbats = self.statline_set.filter(season_name=season_name).aggregate(Sum('at_bats')).values()[0]
+	    sum_singles = self.statline_set.filter(season_name=season_name).aggregate(Sum('singles')).values()[0]
+	    sum_doubles = self.statline_set.filter(season_name=season_name).aggregate(Sum('doubles')).values()[0]
+	    sum_triples = self.statline_set.filter(season_name=season_name).aggregate(Sum('triples')).values()[0]
+	    sum_homeruns = self.statline_set.filter(season_name=season_name).aggregate(Sum('homeruns')).values()[0]
+	    sum_rboe = self.statline_set.filter(season_name=season_name).aggregate(Sum('rboe')).values()[0]  
+	    rboe = (sum_singles*0.9 + sum_rboe*0.92 + sum_doubles*1.24 + 
+	    sum_triples*1.56 + sum_homeruns*1.95) / float(sum_atbats)  
+	    return woba
+	
+	
+
     def __unicode__(self):
         return self.name
     
@@ -62,13 +93,9 @@ class StatLine(models.Model):
     runs = models.IntegerField(max_length=2, choices=NUMBER_CHOICES, default=0)
     rbis = models.IntegerField(max_length=2, choices=NUMBER_CHOICES, default=0)
     rboe = models.IntegerField(max_length=2, choices=NUMBER_CHOICES, default=0)
-    hits = models.IntegerField(max_length=3, blank=True)
-    
-    
-    def save(self):
-        self.hits = self.singles + self.doubles + self.triples + self.homeruns
-        super(StatLine, self).save()
     
     def __unicode__(self):
-	    return self.player.name
-    
+        return self.player.name
+
+   
+        
--- softball/views.py
+++ softball/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render_to_response
 from django.db.models import Sum, Count
 from softball.models import Player, StatLine
+from decimal import Decimal
 
 def stat_table(request):
     
-    
-    stats = Player.objects.filter(statline__season_name="Spring 2012"
-    ).annotate(
-        sum_atbats=Sum('statline__at_bats'),
-        sum_singles=Sum('statline__singles'),
-        sum_doubles=Sum('statline__doubles'),
-        sum_triples=Sum('statline__triples'),
-        sum_homeruns=Sum('statline__homeruns'),
-        sum_rbis=Sum('statline__rbis'),
-        sum_rboe=Sum('statline__rboe'),
-        sum_hits=Sum('statline__hits'),
-    )
-
-    
-
-  
-   
-    return render_to_response('stat_table.html', {
-        'stats': stats,
-       
-    })
+    players = Player.objects.filter(statline__season_name="Spring 2012")
+    return render_to_response('stat_table.html', {'players': players})
+        
+        
